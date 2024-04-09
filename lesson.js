c440459fa8d1e473ee538d8f73b2e5d4c136f45c
JulieKodehode <julie.kodehode@gmail.com>
2024-04-09 11:54:42 +0200

    First commmit

---
+++ lesson.js
@@ -0,0 +1,25 @@
+// if/else statement
+
+let number = 10;
+
+// Operators:
+// = erklærer en verdi
+// == sjekker innhold
+// === sjekker innhold og datatype
+// < mindre enn / less then
+// > større enn / more then
+// & og / and
+// | eller / either 
+
+if (number === 10) {
+    console.log("Dette tallet er akkurat 10");
+} 
+else if (number < 10) {
+    console.log("Dette tallet er mindre enn 10")
+}
+else if (number > 10) {
+    console.log("Dette tallet er større enn 10")
+}
+else {
+    console.log("Dette er ikke et tall")
+}
--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello!');
+const submitValue = document.querySelector("#submitValue");
+console.log(submitValue);
+
+// Når knappen trykkes vil denne 'arrow function' hente data som er skrevet i input.
+submitValue.addEventListener("click", () => {
+    const inputValue = document.querySelector("#inputValue").value;
+    console.log(inputValue);
+
+    const displayValue = document.querySelector("#displayValue");
+    console.log(displayValue);
+
+    displayValue.textContent = inputValue;
+});
